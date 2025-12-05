15347b1e1158cd35b2ec59e9a0fd70b505a94997
Guilherme Tonin <gtoninjorge@gmail.com>
2025-12-05 19:13:17 -0300

    Refatorar Restaurante com classmethod e property

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -2,24 +2,38 @@ class Restaurante:
   restaurantes = []
 
   def __init__(self, nome, tipo):
-    self.nome = nome
-    self.tipo = tipo
-    self.ativo = False
+    self._nome = nome.title() # primeire letra maiuscula
+    self._tipo = tipo.title()
+    # atributo protegido
+    self._ativo = False
     Restaurante.restaurantes.append(self)
 
-  # self - é a referência atual
+  # self é a referência atual
   def __str__(self):
-    return f'{self.nome} | {self.tipo}'
+    return f'{self._nome} | {self._tipo}'
   
-  # método para listar todos os restaurantes
-  def listar_restaurantes():
-    for restaurante in Restaurante.restaurantes:
-      print(f'nome: {restaurante.nome} | tipo: {restaurante.tipo} | status: {restaurante.ativo}')
+  # método da classe para listar todos os restaurantes
+  @classmethod
+  def listar_restaurantes(cls):
+    print(f'{'NOME DO RESTAURANTE'.ljust(25)} | {'TIPO'.ljust(20)} | STATUS')
+
+    for restaurante in cls.restaurantes:
+      print(f'{restaurante._nome.ljust(25)} | {restaurante._tipo.ljust(20)} | {restaurante.ativo}')
+
+  # permite tranformar um método em atributo, podendo acessar sem os ()
+  # habilitando o controle de lógica ou validação na leitura e escrita
+  @property
+  def ativo(self):
+    return '☑' if self._ativo else '☐'
+  
+  def altenar_estado(self):
+    self._ativo = not self._ativo
     
 # dir() - atributos, métodos e propriedades de um objeto
 # vars() - dicionário desses atributos e métodos
 
-restaurante_praca = Restaurante('Praça', 'Italiana')
+restaurante_praca = Restaurante('praça', 'italiana')
+restaurante_praca.altenar_estado()
 restaurante_duze = Restaurante('Duzé', 'Gourmet')
 
 Restaurante.listar_restaurantes()
