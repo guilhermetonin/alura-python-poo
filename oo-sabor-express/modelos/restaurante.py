e22050cffa1c3dc921b83463ad08d762d859aee8
Guilherme Tonin <gtoninjorge@gmail.com>
2025-12-06 19:12:10 -0300

    Implementar sistema de avaliação e média

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
   restaurantes = []
 
@@ -6,6 +8,7 @@ class Restaurante:
     self._tipo = tipo.title()
     # atributo protegido
     self._ativo = False
+    self._avaliacao = []
     Restaurante.restaurantes.append(self)
 
   # self é a referência atual
@@ -15,25 +18,28 @@ class Restaurante:
   # método da classe para listar todos os restaurantes
   @classmethod
   def listar_restaurantes(cls):
-    print(f'{'NOME DO RESTAURANTE'.ljust(25)} | {'TIPO'.ljust(20)} | STATUS')
+    print(f'{'NOME DO RESTAURANTE'.ljust(25)} | {'TIPO'.ljust(20)} | {'AVALIAÇÃO'.ljust(20)} | STATUS')
 
     for restaurante in cls.restaurantes:
-      print(f'{restaurante._nome.ljust(25)} | {restaurante._tipo.ljust(20)} | {restaurante.ativo}')
+      print(f'{restaurante._nome.ljust(25)} | {restaurante._tipo.ljust(20)} | {str(restaurante.media_avaliacao).ljust(20)} | {restaurante.ativo}')
 
-  # permite tranformar um método em atributo, podendo acessar sem os ()
-  # habilitando o controle de lógica ou validação na leitura e escrita
   @property
   def ativo(self):
     return '☑' if self._ativo else '☐'
+    
   
   def altenar_estado(self):
     self._ativo = not self._ativo
-    
-# dir() - atributos, métodos e propriedades de um objeto
-# vars() - dicionário desses atributos e métodos
+  
+  def receber_avaliacao(self, cliente, nota):
+    avaliacao = Avaliacao(cliente, nota)
+    self._avaliacao.append(avaliacao)
 
-restaurante_praca = Restaurante('praça', 'italiana')
-restaurante_praca.altenar_estado()
-restaurante_duze = Restaurante('Duzé', 'Gourmet')
-
-Restaurante.listar_restaurantes()
+  @property
+  def media_avaliacao(self):
+    if not self._avaliacao:
+      return 0
+    soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+    qtde_notas = len(self._avaliacao)
+    media = round(soma_notas / qtde_notas, 1)
+    return media
