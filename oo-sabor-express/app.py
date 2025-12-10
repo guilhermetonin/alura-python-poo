036b060465e1364edaca1401b04fbdae2aeaeac3
Guilherme Tonin <gtoninjorge@gmail.com>
2025-12-10 17:31:19 -0300

    Testar funcionalidades de cardápio e descontos

--- oo-sabor-express/app.py
+++ oo-sabor-express/app.py
@@ -6,9 +6,18 @@ restaurante_praca = Restaurante('praça', 'italiano')
 bebida_suco = Bebida('Suco de Uva', 5, 'Médio')
 prato_pao = Prato('Pão Frânces', 2.50, 'Pãozinho quentinho')
 
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
+
+
 def main():
   print(bebida_suco)
   print(prato_pao)
+  restaurante_praca.exibir_cardapio
+
+  bebida_suco.aplicar_desconto()
+  prato_pao.aplicar_desconto()
+  restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
   main()
